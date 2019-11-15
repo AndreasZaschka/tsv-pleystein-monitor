3c2c6c7828915cb047602a21da4a0788a34a0090
Andreas Zaschka <andreas.zaschka@bindoc.de>
2019-11-15 09:30:10 +0100

    added other leagues to rotation
    Signed-off-by: Andreas Zaschka <andreas.zaschka@bindoc.de>

--- tsv_pleystein_monitor.js
+++ tsv_pleystein_monitor.js
@@ -15,3 +15,48 @@ function getNextSponsorParam(currentSponsorId) {
         return '1';
     }
 }
+
+function showAllLeagues() {
+    const now = new Date();
+    const dayOfWeek = now.getDay();
+
+    // if day is sunday, then game day..
+    if (dayOfWeek === 0) {
+
+        const periodStart = new Date();
+        periodStart.setHours(16, 0, 0);
+        const periodEnd = new Date();
+        periodEnd.setHours(20, 0, 0);
+
+        if (now >= periodStart && now < periodEnd) {
+            console.log("its game day :D");
+            return true;
+        }
+    }
+
+    // if day is wednesday or friday, then training..
+    if (dayOfWeek === 3 || dayOfWeek === 5) {
+
+        const _periodStart = new Date();
+        _periodStart.setHours(9, 0, 0);
+        const _periodEnd = new Date();
+        _periodEnd.setHours(10, 0, 0);
+
+        if (now >= _periodStart && now < _periodEnd) {
+            console.log("its a test :D");
+            return true;
+        }
+
+        const periodStart = new Date();
+        periodStart.setHours(19, 0, 0);
+        const periodEnd = new Date();
+        periodEnd.setHours(22, 0, 0);
+
+        if (now >= periodStart && now < periodEnd) {
+            console.log("its training :D");
+            return true;
+        }
+    }
+
+    return false;
+}
