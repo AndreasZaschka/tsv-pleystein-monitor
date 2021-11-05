6878254266f6472e81d8852b094265795b5341bd
Andreas Zaschka <andreas.zaschka@bindoc.de>
2021-11-05 10:55:47 +0100

    feat: moved pages to src dir

---
+++ src/tsv_pleystein_monitor.js
@@ -0,0 +1,55 @@
+function getSponsorId() {
+    var urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has('sponsorId')) {
+        return this.getNextSponsorParam(urlParams.get('sponsorId'));
+    } else {
+        return '1'
+    }
+}
+
+function getNextSponsorParam(currentSponsorId) {
+
+    var _currentSponsorId = parseInt(currentSponsorId);
+
+    if (_currentSponsorId >= 7) {
+        return '1';
+    } else {
+        return (_currentSponsorId + 1).toString();
+    }
+}
+
+function showAllLeagues() {
+    const now = new Date();
+    const dayOfWeek = now.getDay();
+
+    // if day is sunday, then game day..
+    if (dayOfWeek === 0) {
+
+        const periodStart = new Date();
+        periodStart.setHours(16, 0, 0);
+        const periodEnd = new Date();
+        periodEnd.setHours(20, 0, 0);
+
+        if (now >= periodStart && now < periodEnd) {
+            console.log("its game day :D");
+            return true;
+        }
+    }
+
+    // if day is wednesday or friday, then training..
+    if (dayOfWeek === 3 || dayOfWeek === 5) {
+
+        const periodStart = new Date();
+        periodStart.setHours(19, 0, 0);
+        const periodEnd = new Date();
+        periodEnd.setHours(22, 0, 0);
+
+        if (now >= periodStart && now < periodEnd) {
+            console.log("its training :D");
+            return true;
+        }
+    }
+
+    return false;
+}
